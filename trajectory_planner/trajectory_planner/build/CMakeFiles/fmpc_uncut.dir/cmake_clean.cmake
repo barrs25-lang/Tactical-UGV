faf5d071c5e9623ffa2ca876f755a87af0b8ef0b
Barr Shlagman <bshlag11@gmail.com>
2026-03-08 16:23:42 -0400

    This is all of the original codebase from Julius

---
+++ trajectory_planner/trajectory_planner/build/CMakeFiles/fmpc_uncut.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fmpc_uncut.dir/src/f_mpc_uncut/f_mpc_uncut.cpp.o"
+  "CMakeFiles/fmpc_uncut.dir/src/f_mpc_uncut/f_mpc_uncut.cpp.o.d"
+  "CMakeFiles/fmpc_uncut.dir/src/line_search/line_search.cpp.o"
+  "CMakeFiles/fmpc_uncut.dir/src/line_search/line_search.cpp.o.d"
+  "CMakeFiles/fmpc_uncut.dir/src/logging/logging.cpp.o"
+  "CMakeFiles/fmpc_uncut.dir/src/logging/logging.cpp.o.d"
+  "CMakeFiles/fmpc_uncut.dir/src/main/main.cpp.o"
+  "CMakeFiles/fmpc_uncut.dir/src/main/main.cpp.o.d"
+  "fmpc_uncut"
+  "fmpc_uncut.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fmpc_uncut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ trajectory_planner/trajectory_planner/build/CMakeFiles/fmpc_uncut.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for fmpc_uncut.
+# This may be replaced when dependencies are built.
